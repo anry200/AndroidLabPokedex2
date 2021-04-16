9b2593180bd95edd6242dbc6a480babde21360b3
Denys Honcharenko <anry200@gmail.com>
2021-04-16 14:21:51 +0300

    [Lesson 2]: rxjava wrapper example

--- app/src/main/java/com/example/androidlabpokedex2/data/NetworkPokemonRepository.kt
+++ app/src/main/java/com/example/androidlabpokedex2/data/NetworkPokemonRepository.kt
@@ -7,6 +7,7 @@ import com.example.androidlabpokedex2.data.network.PokemonPartialResponse
 import com.example.androidlabpokedex2.domain.RepositoryCallback
 import com.example.androidlabpokedex2.domain.PokemonEntity
 import com.example.androidlabpokedex2.domain.PokemonRepository
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,11 +22,6 @@ class NetworkPokemonRepository(
 
                 if (response.isSuccessful && pokemonListResponse != null) {
                     val pokemonList = pokemonListResponse.results.map {
-
-
-                        //
-
-
                         PokemonEntity(it.name, it.name, generateUrlFromId(it.name))
                     }
                     callback.onSuccess(pokemonList)
@@ -40,29 +36,38 @@ class NetworkPokemonRepository(
         })
     }
 
-    override fun getPokemonById(id: String, callback: RepositoryCallback<PokemonEntity>) {
-        api.fetchPokemonInfo(id).enqueue(object : Callback<PokemonDetailedResponse> {
-            override fun onResponse(call: Call<PokemonDetailedResponse>, response: Response<PokemonDetailedResponse>) {
-                val serverPokemon: PokemonDetailedResponse? = response.body()
+    override fun getPokemonById(id: String): Single<PokemonEntity> {
 
-                if (serverPokemon == null) {
-                    callback.onError("Empty response")
-                } else {
-                    val abilities = serverPokemon.abilities.map { it.ability.name }
+        return Single.create { emitter ->
+            api.fetchPokemonInfo(id).enqueue(object : Callback<PokemonDetailedResponse> {
+                override fun onResponse(call: Call<PokemonDetailedResponse>, response: Response<PokemonDetailedResponse>) {
+                    val serverPokemon: PokemonDetailedResponse? = response.body()
 
-                    val pokemonEntity = PokemonEntity(
-                        id = serverPokemon.id,
-                        name = serverPokemon.name,
-                        previewUrl = generateUrlFromId(serverPokemon.id),
-                        abilities = abilities)
-                    callback.onSuccess(pokemonEntity)
+                    if (serverPokemon == null) {
+                        if (!emitter.isDisposed) {
+                            emitter.onError(Throwable("Empty response"))
+                        }
+                    } else {
+                        val abilities = serverPokemon.abilities.map { it.ability.name }
+
+                        val pokemonEntity = PokemonEntity(
+                            id = serverPokemon.id,
+                            name = serverPokemon.name,
+                            previewUrl = generateUrlFromId(serverPokemon.id),
+                            abilities = abilities)
+                        if (!emitter.isDisposed) {
+                            emitter.onSuccess(pokemonEntity)
+                        }
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<PokemonDetailedResponse>, t: Throwable) {
-                callback.onError("Error ${t.message}")
-            }
-        })
+                override fun onFailure(call: Call<PokemonDetailedResponse>, t: Throwable) {
+                    if (!emitter.isDisposed) {
+                        emitter.onError(Throwable("Empty response"))
+                    }
+                }
+            })
+        }
     }
 
     fun generateUrlFromId(id: String): String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"

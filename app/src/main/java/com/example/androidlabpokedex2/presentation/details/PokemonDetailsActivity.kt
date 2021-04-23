c38c777f9fabbe006b58c73981908119890c3278
Denys Honcharenko <anry200@gmail.com>
2021-04-23 13:48:05 +0300

    [Lesson 5]: finished Details flow

--- app/src/main/java/com/example/androidlabpokedex2/presentation/details/PokemonDetailsActivity.kt
+++ app/src/main/java/com/example/androidlabpokedex2/presentation/details/PokemonDetailsActivity.kt
@@ -3,11 +3,18 @@ package com.example.androidlabpokedex2.presentation.details
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.androidlabpokedex2.R
+import com.squareup.picasso.Picasso
 
 class PokemonDetailsActivity: AppCompatActivity() {
+    private val viewModel = PokemonDetailsViewModel()
 
     companion object {
         private const val PARAM_POKEMON_ID = "Pockemon_Id"
@@ -24,7 +31,53 @@ class PokemonDetailsActivity: AppCompatActivity() {
 
         val id = intent.extras?.getString(PARAM_POKEMON_ID)
 
+        if (id != null) {
+            loadPokemonData(id)
+        } else {
+            Log.d("TAG, ","Error, pokemon with id=$id not found")
+            finish()
+        }
+    }
+
+    private fun loadPokemonData(id: String) {
+        viewModel.loadPokemonById(id)
+
+        val progressView = findViewById<ProgressBar>(R.id.progress)
+        val contentView = findViewById<View>(R.id.content_group)
+        val errorView = findViewById<TextView>(R.id.error_message_text)
+
+        viewModel.viewState().observe(this) { viewState ->
+            when(viewState) {
+                PokemonDetailsViewState.Loading -> {
+                    progressView.isVisible = true
+                    contentView.isVisible = false
+                    errorView.isVisible = false
+                }
+                is PokemonDetailsViewState.Data -> {
+                    progressView.isVisible = false
+                    contentView.isVisible = true
+                    errorView.isVisible = false
+
+                    showDataState(state = viewState)
+                }
+                is PokemonDetailsViewState.Error -> {
+                    progressView.isVisible = false
+                    contentView.isVisible = false
+                    errorView.isVisible = true
+                }
+            }
+        }
+    }
+
+    private fun showDataState(state: PokemonDetailsViewState.Data) {
         val nameTextView = findViewById<TextView>(R.id.name)
-        nameTextView.text = id
+        val imagePreview = findViewById<ImageView>(R.id.image)
+        val abilitiesTextView = findViewById<TextView>(R.id.abilities)
+
+        nameTextView.text = state.name
+
+        Picasso.get().load(state.imageUrl).into(imagePreview)
+
+        abilitiesTextView.text = state.abilities.joinToString(separator = ",") { it }
     }
 }
--- app/src/main/java/com/example/androidlabpokedex2/presentation/details/PokemonDetailsViewModel.kt
+++ app/src/main/java/com/example/androidlabpokedex2/presentation/details/PokemonDetailsViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.androidlabpokedex2.presentation.details
 
-class PokemonDetailsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.androidlabpokedex2.di.Injector
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class PokemonDetailsViewModel: ViewModel() {
+    private val repository = Injector.providePokemonRepository()
+    private var disposable: Disposable? = null
+    private val viewStateLiveData = MutableLiveData<PokemonDetailsViewState>()
+
+    fun viewState(): LiveData<PokemonDetailsViewState> = viewStateLiveData
+
+    fun loadPokemonById(id: String) {
+        viewStateLiveData.value = PokemonDetailsViewState.Loading
+
+        disposable = repository.getPokemonById(id)
+            .delay(2, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ pokemonEntity ->
+                viewStateLiveData.value = PokemonDetailsViewState.Data(
+                    name = pokemonEntity.name,
+                    imageUrl = pokemonEntity.previewUrl,
+                    abilities = pokemonEntity.abilities
+                )
+            }, {
+                viewStateLiveData.value = PokemonDetailsViewState.Error("Failed to load pokemon with id=$id")
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable?.dispose()
+    }
 }
